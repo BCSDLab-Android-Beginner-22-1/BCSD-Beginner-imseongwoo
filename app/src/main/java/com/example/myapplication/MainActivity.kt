46b823f05e88f39ba4f2028dac8c76df42ab629b
임성우 <seongwoorang@naver.com>
2022-05-14 02:09:06 +0900

    RecyclerView 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = mutableListOf<String>()
+        val editTextId = findViewById<EditText>(R.id.edittext_name)
+        val plusButton = findViewById<Button>(R.id.plus_button)
+        val editTextContent = editTextId.text
+        val rv = findViewById<RecyclerView>(R.id.rv)
+
+        plusButton.setOnClickListener {
+            items.add(editTextContent.toString())
+        }
+
+        val rvAdapter = RVAdapter(items)
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(this)
+
+        rvAdapter.itemClick = object : RVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                Toast.makeText(baseContext, items[position], Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
 }
+
